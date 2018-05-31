975d924a63512e3f14b862e38d9a36859d1134a4
Gabriel Guimarães <gabriel.oliveira@ccc.ufcg.edu.br>
2018-05-30 21:22:37 -0300

    package com.pharmaweb.pharmaweb.interceptor

---
+++ pharmaweb/src/main/kotlin/com/pharmaweb/pharmaweb/config/FirebaseConfiguration.kt
@@ -0,0 +1,30 @@
+package com.pharmaweb.pharmaweb.config
+
+import com.google.firebase.FirebaseApp
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.Resource
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseOptions
+import org.springframework.core.io.ClassPathResource
+
+@Configuration
+@ComponentScan
+class FirebaseConfiguration {
+
+    @Bean
+    fun firebaseBuilderApp(): FirebaseApp {
+
+        val resource: Resource = ClassPathResource("pharmaweb-firebase-key.json");
+
+        val options = FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(resource.inputStream))
+                .setDatabaseUrl("https://pharmaweb-35ccb.firebaseio.com")
+                .build()
+
+        return FirebaseApp.initializeApp(options)
+
+    }
+
+}
