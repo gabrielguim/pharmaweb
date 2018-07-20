885859d503aedb9c2fce32fd6445be2bd356117c
Gabriel Guimarães <gabriel.oliveira@ccc.ufcg.edu.br>
2018-07-19 22:26:23 -0300

    adding swagger2 to auto generate the API Docs

--- pharmaweb/src/main/kotlin/com/pharmaweb/pharmaweb/routing/DruggistRouting.kt
+++ pharmaweb/src/main/kotlin/com/pharmaweb/pharmaweb/routing/DruggistRouting.kt
@@ -2,30 +2,38 @@ package com.pharmaweb.pharmaweb.routing
 
 import com.pharmaweb.pharmaweb.model.Druggist
 import com.pharmaweb.pharmaweb.service.DruggistService
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
 @RestController
 @RequestMapping("/druggists")
+@Api(value = "Druggist Endpoint", description = "Druggist CRUD operations")
 class DruggistRouting {
 
     @Autowired
     lateinit var druggistService: DruggistService
 
+    @ApiOperation(value = "Returns all the registered druggists")
     @GetMapping
     fun getAll() = druggistService.getAll()
 
+    @ApiOperation(value = "Registers a new given druggist")
     @PostMapping
     fun register(@Valid @RequestBody druggist: Druggist) = druggistService.register(druggist)
 
+    @ApiOperation(value = "Returns a single druggist that has the given id")
     @GetMapping("/{id}")
     fun findById(@PathVariable(value = "id") druggistId: String) = druggistService.findById(druggistId)
 
+    @ApiOperation(value = "Alters a single druggist using the new given druggists that has the given id")
     @PutMapping("/{id}")
     fun alter(@PathVariable(value = "id") druggistId: String,
               @Valid @RequestBody newDruggist: Druggist) = druggistService.alter(druggistId, newDruggist)
 
+    @ApiOperation(value = "Deletes a single druggist that has the given id")
     @DeleteMapping("/{id}")
     fun delete(@PathVariable(value = "id") druggistId: String) = druggistService.delete(druggistId)
 
--- pharmaweb/src/main/kotlin/com/pharmaweb/pharmaweb/routing/ProductRouting.kt
+++ pharmaweb/src/main/kotlin/com/pharmaweb/pharmaweb/routing/ProductRouting.kt
@@ -2,6 +2,8 @@ package com.pharmaweb.pharmaweb.routing
 
 import com.pharmaweb.pharmaweb.model.Product
 import com.pharmaweb.pharmaweb.service.ProductService
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 import java.util.*
@@ -9,30 +11,38 @@ import javax.validation.Valid
 
 @RestController
 @RequestMapping("/products")
+@Api(value = "Product Endpoint", description = "Product CRUD operations")
 class ProductRouting {
 
     @Autowired
     lateinit var productService: ProductService
 
+    @ApiOperation(value = "Returns all the registered products")
     @GetMapping
     fun getAll() = productService.getAll()
 
+    @ApiOperation(value = "Registers a new given product")
     @PostMapping
     fun register(@Valid @RequestBody product: Product) = productService.register(product)
 
+    @ApiOperation(value = "Returns a list of products that have in their attributes the given text")
     @GetMapping("/")
     fun searchByText(@RequestParam("q") textToSearch: String) = productService.searchByText("%${textToSearch.toLowerCase()}%")
 
+    @ApiOperation(value = "Returns a list of grouped products, it can be by category or department")
     @GetMapping("/group/")
     fun groupBy(@RequestParam("by") group: String) = productService.groupBy(group)
 
+    @ApiOperation(value = "Returns a single product that has the given id")
     @GetMapping("/{id}")
     fun findById(@PathVariable(value = "id") productId: String) = productService.findById(productId)
 
+    @ApiOperation(value = "Alters a single product using the new given product that has the given id")
     @PutMapping("/{id}")
     fun alter(@PathVariable(value = "id") productId: String,
               @Valid @RequestBody newProduct: Product) = productService.alter(productId, newProduct)
 
+    @ApiOperation(value = "Deletes a single product that has the given id")
     @DeleteMapping("/{id}")
     fun delete(@PathVariable(value = "id") productId: String) = productService.delete(productId)
 
